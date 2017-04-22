4c0b6914152c8ac326aa2862f19b8c969ae1d82c
Wilfrido Nuqui <dofreewill22@gmail.com>
2017-04-22 14:57:22 +0800

    Refactor `init_config_store` task and add tests

--- lib/mix/tasks/maintenance.init_config_store.ex
+++ lib/mix/tasks/maintenance.init_config_store.ex
@@ -1,36 +1,61 @@
-require Logger
-
 defmodule Mix.Tasks.Maintenance.InitConfigStore do
   use Mix.Task
+  import Plug.OnMaintenance.Util, only: [on_maintenance_db: 0]
 
   @shortdoc "Create .on_maintenance.sqlite3 database."
-  @recursive true
 
   @moduledoc """
   Creates ".on_maintenance.sqlite3" database which contains only 1 table.
-  The table, named "on_maintenance_configs", is initialized with 1 row of data:
-  - **0** value for **on_maintenance** means the application is not in maintenance mode.
-  - **1** value for **retry_after** no `retry-after` header for 503 response.
+
+  Table ".on_maintenance_configs" will be initialized with 1 row of data:
+  - **0** value for **on_maintenance** column means the application is not in maintenance mode.
+  - **0** value for **retry_after** column means **0** `retry-after` header for 503 response.
+    This value is not applicable since **on_maintenance** is **0**.
+    When **on_maintenance** is **1**, **retry_after** must be **> 0**.
+    This value is the expected duration of maintenance in seconds.
 
   ## Example
       mix maintenance.init_config_store
   """
 
   def run(_argv) do
-    {:ok, _} = File.open Plug.OnMaintenance.Util.on_maintenance_db(), [:write]
+    {:ok, _}  = File.open(on_maintenance_db(), [:write])
+    {:ok, db} = Sqlitex.open(on_maintenance_db())
 
-    {:ok, db} = Sqlitex.open(Plug.OnMaintenance.Util.on_maintenance_db())
+    :ok = Sqlitex.exec(db, create_table_sql())
+    :ok = Sqlitex.exec(db, insert_record_sql())
 
-    :ok = Sqlitex.exec(db, "CREATE TABLE on_maintenance_configs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, on_maintenance INTEGER, retry_after INTEGER, created_at datetime NOT NULL, updated_at datetime NOT NULL)")
+    records = Sqlitex.query!(db, select_sql())
+    records = Enum.map(records, &(&1[:on_maintenance]))
 
-    now = DateTime.utc_now() |> DateTime.to_iso8601()
-    :ok = Sqlitex.exec(db, "INSERT INTO on_maintenance_configs (on_maintenance, retry_after, created_at, updated_at) VALUES (0, 0, '" <> now <> "', '" <> now <> "')")
-
-    query = "SELECT id, on_maintenance FROM on_maintenance_configs ORDER BY id DESC LIMIT 1"
-    rows = Enum.map(Sqlitex.query!(db, query), &(&1[:on_maintenance]))
-
-    if hd(rows) == 0 do
-      Logger.info "sqlite db created to store application maintenance configurations."
+    if hd(records) == 0 do
+      Mix.shell.info """
+      .on_maintenance.sqlite3 db is created to store "on maintenance" configurations
+      """
     end
+  end
+
+  defp create_table_sql do
+    """
+    CREATE TABLE on_maintenance_configs (
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      on_maintenance INTEGER NOT NULL,
+      retry_after INTEGER NOT NULL,
+      created_at datetime NOT NULL,
+      updated_at datetime NOT NULL
+    )
+    """
+  end
+
+  defp insert_record_sql do
+    now = DateTime.utc_now() |> DateTime.to_iso8601()
+
+    "INSERT INTO on_maintenance_configs ("                  <> "\n" <> \
+    "  on_maintenance, retry_after, created_at, updated_at" <> "\n" <> \
+    ") " <>  "VALUES (0, 0, '" <> now <> "', '" <> now <> "')"
+  end
+
+  defp select_sql do
+    "SELECT id, on_maintenance FROM on_maintenance_configs ORDER BY id DESC LIMIT 1"
   end
 end
